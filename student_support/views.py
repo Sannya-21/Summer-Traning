b54c2a20012266f2ecc8a1367743c4480666c1b0
Sannya-21 <muzaffarsanniya561@gmail.com>
2026-06-28 23:16:24 +0530

    complain form

--- student_support/views.py
+++ student_support/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ComplaintForm
+from .models import Complaint
 
 def home(request):
     return render(request, 'home.html')
@@ -14,3 +15,7 @@ def complaint(request):
         form = ComplaintForm()
 
     return render(request, 'complaint.html', {'form': form})
+
+    def complaint_list(request):
+        complaints = Complaint.objects.all()
+        return render(request, 'complaint_list.html', {'complaints': complaints})
