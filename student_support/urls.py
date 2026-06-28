189dec10799c7ed1cde5cb708d21c3069d4cc74d
Sannya-21 <muzaffarsanniya561@gmail.com>
2026-06-28 22:08:51 +0530

    branch shift

--- student_support/urls.py
+++ student_support/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('complaint/', views.complaint, name='complaint'),
 ]
--- student_support/views.py
+++ student_support/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ComplaintForm
 
 def home(request):
     return render(request, 'home.html')
+
+def complaint(request):
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ComplaintForm()
+
+    return render(request, 'complaint.html', {'form': form})
